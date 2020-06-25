ba1a1640ab0fd658547a824b8cf8bed06b0376f2
okamos <himinato.k@gmail.com>
2020-06-25 21:01:54 +0900

    :hammer: Performance

--- src/main/java/com/okamos/Daybreak/listeners/BedListener.java
+++ src/main/java/com/okamos/Daybreak/listeners/BedListener.java
@@ -4,6 +4,7 @@ import com.okamos.Daybreak.Daybreak;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
@@ -12,29 +13,44 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+
+import static java.util.stream.Collectors.toList;
 
 public class BedListener implements Listener {
   public static final Map<UUID, BukkitTask> Tasks = new HashMap<>();
 
   @EventHandler
   public void onBedEnter(final PlayerBedEnterEvent event) {
-    BukkitTask task = Bukkit.getScheduler().runTaskLater(Daybreak.getDaybreak(), new Runnable() {
+    World world = event.getBed().getWorld();
+    BukkitTask task = Bukkit.getScheduler().runTaskTimer(Daybreak.getDaybreak(), new Runnable() {
       @Override
       public void run() {
-        World world = event.getBed().getWorld();
+        if (getSleeping(world).size() == 0) {
+          Tasks.get(event.getBed().getWorld().getUID()).cancel();
+          return;
+        }
         // is night?
         if (world.getTime() > 12950 || world.getTime() < 23950) {
           world.setTime(0);
         }
       }
-    }, 10 * 20L);
-    Tasks.put(event.getPlayer().getUniqueId(), task);
+    }, 10 * 20L, 10 * 20L);
+    Tasks.put(world.getUID(), task);
   }
 
   @EventHandler
   public void onBedLeave(final PlayerBedLeaveEvent event) {
-    Tasks.get(event.getPlayer().getUniqueId()).cancel();
+    World world = event.getBed().getWorld();
+    final int sleeping = getSleeping(world).size();
+    if (sleeping == 0) {
+      Tasks.get(event.getBed().getWorld().getUID()).cancel();
+    }
+  }
+
+  public static List<Player> getSleeping(final World world) {
+    return world.getPlayers().stream().filter(Player::isSleeping).collect(toList());
   }
 }
